995818079a79266cfaaf0302a8fb8aa5266c43d0
Sena <antoniocarlossenajunior@gmail.com>
2024-06-10 17:17:30 -0300

    atualizado

--- GuanabaraDesafios/desafio055.py
+++ GuanabaraDesafios/desafio055.py
@@ -1,16 +1,32 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-lista_peso = []
-for cont in range (0,5):
-    peso = float(input('Digite seu peso: '))
-    lista_peso.append(peso)
+maior = 0
+menor = 0
 
-print(lista_peso)
+#lista_peso = []
+for cont in range (1,6):
+    peso = float(input(f'Digite o peso da {cont}ª pessoa: '))
+    if cont == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso 
+    #lista_peso.append(peso)
 
-maior_peso = max(lista_peso)
-menor_peso = min(lista_peso)
+#print(lista_peso)
+
+#maior_peso = max(lista_peso)
+#menor_peso = min(lista_peso)
+
+#print(f'''
+#O maior peso é {maior_peso:.1f} Kg
+#O menor peso é {menor_peso:.1f} Kg
+#''')
 
 print(f'''
-O maior peso é {maior_peso:.1f} Kg
-O menor peso é {menor_peso:.1f} Kg
+O maior peso é {maior:.1f} Kg
+O menor peso é {menor:.1f} Kg
 ''')
